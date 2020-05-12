3fb782398e1461186f55843e4822ca07016e8391
aisolab <bsk0130@gmail.com>
2020-05-13 00:52:45 +0900

    init

---
+++ src/main/scala/com/allaboutscala/one/tutorial_04/HelloWorld.scala
@@ -0,0 +1,7 @@
+package com.allaboutscala.one.tutorial_04
+
+// http://allaboutscala.com/tutorials/chapter-1-getting-familiar-intellij-ide/scala-tutorial-first-hello-world-application/
+
+object HelloWorld extends App {
+  println("Hello World from Scala!")
+}
